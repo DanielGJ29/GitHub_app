098a0b3d8e6a880ae918885aad91b504bc0a8997
Daniel <daniel.zam29@gmail.com>
2021-11-19 00:17:20 -0600

    load

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Loader from '../Components/Custom/Loader/Loader';
 import Home from '../Pages/Home/Home';
 
 const Layout = ({children}) => {
     return (
         <div>
-            <Loader/>
             <Home/>
             {children}
         </div>
